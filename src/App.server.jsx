cb9aa359e3592b533311eb86f2b428606cb33bb2
Brian Smith <smithbh129@gmail.com>
2022-10-19 20:32:06 -0600

    Initial commit generated by Hydrogen

---
+++ src/App.server.jsx
@@ -0,0 +1,59 @@
+import {Suspense} from 'react';
+import renderHydrogen from '@shopify/hydrogen/entry-server';
+import {
+  FileRoutes,
+  PerformanceMetrics,
+  PerformanceMetricsDebug,
+  Route,
+  Router,
+  ShopifyAnalytics,
+  ShopifyProvider,
+  CartProvider,
+  useSession,
+  useServerAnalytics,
+} from '@shopify/hydrogen';
+import {HeaderFallback, EventsListener} from '~/components';
+import {DefaultSeo, NotFound} from '~/components/index.server';
+
+function App({request}) {
+  const pathname = new URL(request.normalizedUrl).pathname;
+  const localeMatch = /^\/([a-z]{2})(\/|$)/i.exec(pathname);
+  const countryCode = localeMatch ? localeMatch[1] : undefined;
+
+  const isHome = pathname === `/${countryCode ? countryCode + '/' : ''}`;
+
+  const {customerAccessToken} = useSession();
+
+  useServerAnalytics({
+    shopify: {
+      isLoggedIn: !!customerAccessToken,
+    },
+  });
+
+  return (
+    <Suspense fallback={<HeaderFallback isHome={isHome} />}>
+      <EventsListener />
+      <ShopifyProvider countryCode={countryCode}>
+        <CartProvider
+          countryCode={countryCode}
+          customerAccessToken={customerAccessToken}
+        >
+          <Suspense>
+            <DefaultSeo />
+          </Suspense>
+          <Router>
+            <FileRoutes
+              basePath={countryCode ? `/${countryCode}/` : undefined}
+            />
+            <Route path="*" page={<NotFound />} />
+          </Router>
+        </CartProvider>
+        <PerformanceMetrics />
+        {import.meta.env.DEV && <PerformanceMetricsDebug />}
+        <ShopifyAnalytics cookieDomain="hydrogen.shop" />
+      </ShopifyProvider>
+    </Suspense>
+  );
+}
+
+export default renderHydrogen(App);
